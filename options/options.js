77aca4b28737f14d7a11c890b5dcb28f32c6fa51
Crowdbotics <team@crowdbotics.com>
2023-10-05 22:23:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiny-grass-43779",
+  url: "https://tiny-grass-43779.botics.co",
+  api: "https://tiny-grass-43779.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
